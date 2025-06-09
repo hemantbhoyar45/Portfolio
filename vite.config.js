d89f07a94cf0409932e32a8fc79ffcefa05afcc6
Yash Atkari <yashatkari7@gmail.com>
2025-06-09 10:00:39 +0530

    optimized

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'gzip',       // You can also use 'brotliCompress'
+      ext: '.gz',              // File extension
+      deleteOriginFile: false, // Keep original files
+    })
+  ],
 })
