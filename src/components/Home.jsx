f5c8a7e0bf2a6815e5ab11e06f013bcded85fd4a
Yash Atkari <yashatkari7@gmail.com>
2025-06-09 10:07:47 +0530

    Lottie Animations Removed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,7 +43,6 @@ export default function Home() {
                             </p>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4 text-center">
-                            <Hero />
                         </div>
                     </div>
                 </div>
